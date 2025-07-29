2cda579d34e8a61ce82ecea651771536f68d9370
RESHU JAYANT <148264967+Rishurajput8279@users.noreply.github.com>
2025-07-29 05:03:01 +0000

    variable and datatype

---
+++ 01_basis/01_variable.js
@@ -0,0 +1,24 @@
+const accountId= 144553
+let accountEmail="rishurajput070@gmail.com"
+var accountPassword="12345"
+let accountState;
+
+accountCity="Jaipur"
+
+// not allowed
+// accountId=2
+accountEmail="ABC@gmail.com"
+accountPassword="54321"
+
+// not recommended
+// accountCity="bengaluru"
+
+if(true){
+    let accountEmail="radhika"
+    var accountPassword=1234
+    console.log(accountEmail);
+    console.log(accountPassword);
+}
+
+console.table([accountId, accountEmail,accountPassword, accountState])
+
---
+++ 01_basis/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3+3) ;  
+console.log("Rishu")
+
+
+let n=1234
+console.log(typeof n)
