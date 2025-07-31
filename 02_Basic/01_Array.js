9b463e4ed6716275e47a10e0ce4413a5922c5b99
Reshu Jaynt <reshu.22b1531032@abes.ac.in>
2025-07-31 12:14:31 +0530

    newLearning

---
+++ 02_Basic/01_Array.js
@@ -0,0 +1,60 @@
+const myArray= [1,2,3,4,5]
+const Heros=["spiderMan","naagraj","shaktiman"]
+
+
+const myArray2= new Array(1,2,3,4,5);
+
+
+
+
+// Array Methord
+
+
+
+// myArray.push(6);
+// myArray.push(7);
+
+
+// console.log(myArray)
+// myArray.pop();
+// console.log(myArray)
+
+
+// myArray.unshift(9)
+
+// console.log(myArray)
+
+// myArray.shift()
+
+// console.log(myArray)
+
+
+// console.log(myArray.includes(9))
+
+// console.log(myArray.indexOf(9))
+
+
+
+// const newArray = myArray.join()
+// console.log(typeof myArray)
+// console.log(typeof newArray)
+
+
+
+// slice and splice
+
+
+console.log("A" ,myArray)
+
+const myn1= myArray.slice(1,3)
+console.log(myn1)
+
+console.log("B" , myArray)
+
+
+const myn2= myArray.splice(1,3)
+console.log("C" , myArray)
+console.log(myn2)
+
+
+
